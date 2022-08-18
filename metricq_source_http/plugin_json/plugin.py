2112f41fa2a45e795b1b2ade043a1f5f5f7a65d3
Mario Bielert <mario.bielert@tu-dresden.de>
2022-08-18 10:35:11 +0200

    reformat using black and isort

--- metricq_source_http/plugin_json/plugin.py
+++ metricq_source_http/plugin_json/plugin.py
@@ -1,14 +1,14 @@
 import json
+
 import jsonpath_rw
 
 
 def response_parse(response, **kwargs):
     json_data = json.loads(response)
-    if not 'json_path' in kwargs:
+    if not "json_path" in kwargs:
         raise Exception('missing required parameter "json_path"')
-    jsonpath_expr = jsonpath_rw.parse(kwargs['json_path'])
+    jsonpath_expr = jsonpath_rw.parse(kwargs["json_path"])
     value_list = jsonpath_expr.find(json_data)
     if not len(value_list):
-        raise Exception('no value found')
+        raise Exception("no value found")
     return value_list[0].value
-
--- metricq_source_http/plugin_sidecooler/plugin.py
+++ metricq_source_http/plugin_sidecooler/plugin.py
@@ -1,10 +1,13 @@
 import json
 import re
 from html.parser import HTMLParser
-NaN = float('nan')
+
+NaN = float("nan")
+
 
 class SC_HTML(HTMLParser):
     kvs = {}
+
     def handle_data(self, data):
         if data.strip():
             sanitized = re.sub(r"\s", "", data)
@@ -14,19 +17,20 @@ class SC_HTML(HTMLParser):
                 value = mo.group(2).strip()
                 self.kvs[key] = value
 
+
 def response_parse(response, **kwargs):
     key = kwargs["key"]
     data_type = kwargs["data_type"]
     parser = SC_HTML()
     parser.feed(response)
     if key in parser.kvs:
-       raw_value = parser.kvs[key]
-       if data_type.lower() == "number":
-          return float(raw_value)
-       elif data_type.lower() == "absolute":
-          return abs(float(raw_value))
-       elif data_type.lower() == "absolute_kilo":
-                 return abs(float(raw_value)*1000)
-       elif data_type.lower() == "state":
-          return int(raw_value == "Yes")
+        raw_value = parser.kvs[key]
+        if data_type.lower() == "number":
+            return float(raw_value)
+        elif data_type.lower() == "absolute":
+            return abs(float(raw_value))
+        elif data_type.lower() == "absolute_kilo":
+            return abs(float(raw_value) * 1000)
+        elif data_type.lower() == "state":
+            return int(raw_value == "Yes")
     return NaN
